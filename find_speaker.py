65bddd99eb8f63bcacaa465da36b11c3fa3c01de
ClarenceWu01 <kela0826@gmail.com>
2024-08-08 18:00:17 +0800

    Add files via upload
    If ran along, this will just go through everything and box it, or say why it is filtered

--- find_speaker.py
+++ find_speaker.py
@@ -5,6 +5,7 @@ import math
 import os
 from numpy import dot
 from numpy.linalg import norm
+import time
 
 
 from scipy.spatial.distance import cosine
@@ -22,7 +23,12 @@ def get_video_files(folder_path, extensions=['.mp4', '.avi', '.mkv']):
 def extract(image_path):
     #temp_path = image_path + ".temp.jpg"
     #adjustable value: confidence
-    faces = DeepFace.extract_faces(img_path=image_path, detector_backend = 'retinaface')
+    #called by: find speaker
+    min_size=200
+    try:
+        faces = DeepFace.extract_faces(img_path=image_path, detector_backend = 'retinaface')
+    except:
+        return ["no", "no"]
     ret = []
     for i in range(len(faces)):
         face = faces[i]
@@ -34,26 +40,49 @@ def extract(image_path):
         face_representation = DeepFace.represent(img_path=temp_path, model_name = "ArcFace", enforce_detection=False)
         #print(face_representation)
         #testing remove print(face_representation)
-        #don't change confidence, I think it is unnecessary. size caps change at your own discretion
-        if(confidence>=0.8 and facial_area['w']>=0 and facial_area['h']>=0):
+        #don't change confidence, I think changing is unnecessary. size caps change at your own discretion
+        if(confidence>=0.8 and facial_area['w']>=min_size and facial_area['h']>=min_size):
             ret.append([face_representation[0]['embedding'], facial_area, 0]) #the final 0 is for the multi-frame determination system only, frames of appearence
-        #the following line is strictly for debugging
         os.remove(temp_path)
-    #the following line is strictly for debugging
+    
     return ret
 
+def extract_test(image_path):
+
+    try:
+        faces = DeepFace.extract_faces(img_path=image_path, detector_backend = 'retinaface')
+    except:
+        return []
+    ret = []
+    for i in range(len(faces)):
+        face = faces[i]
+        confidence = face['confidence']
+        temp_path = image_path + ".temp.jpg"
+        face_array = face['face']
+        cv.imwrite(temp_path, face_array)
+        facial_area = face['facial_area']
+        face_representation = DeepFace.represent(img_path=temp_path, model_name = "ArcFace", enforce_detection=False)
+        if(confidence>=0.8):
+            ret.append([face_representation[0]['embedding'], facial_area, facial_area['w'], facial_area['h']]) #the final 0 is for the multi-frame determination system only, frames of appearence
+        os.remove(temp_path)
+    return ret
+
+
+    
 def compare_face(embedding1, embedding2):
     #the print statements is for testing
+    #called by: none
     cosine_similarity = dot(embedding1, embedding2) / (norm(embedding1) * norm(embedding2))
-    return cosine_similarity>=0.6
+    return cosine_similarity>=0.7
 
-def compare_face1(target, faces):
-    #it is intended that the target would be the candidate being looked as. the 0.7 criteria is flexible
+def compare_faces(target, faces):
+    #it is intended that the target would be the embedding of the candidate being looked as. the 0.7 criteria is flexible
+    #called by: processVid, check_face
     max_match = 0.7
     max_index = -1
     for i in range(len(faces)):
         face=faces[i]
-        cosine_similarity = dot(target[0], face[0]) / (norm(target[0]) * norm(face[0]))
+        cosine_similarity = dot(target, face[0]) / (norm(target) * norm(face[0]))
         if cosine_similarity>max_match:
             max_match = cosine_similarity
             max_index = i
@@ -63,6 +92,7 @@ def compare_face1(target, faces):
 def add_diff_face(known, new_face):
     #this function is also to for multi-frame determination. the result would account for movement and appereance times.
     #the face format can be found in the extract method
+    #called by: none
     similar = False
     for face in known:
         if compare_face(face[0], new_face[0]):
@@ -71,9 +101,10 @@ def add_diff_face(known, new_face):
         known.append(new_face)
 
 def check_face(candidates, faces):
+    #called by: find_speaker
     for i in range(len(candidates)):
         candidate = candidates[i]
-        result = compare_face1(candidate, faces)
+        result = compare_faces(candidate[0], faces)
         if(result>-1):
             face = faces[result]
             candidate[1]=face[1]
@@ -82,7 +113,8 @@ def check_face(candidates, faces):
     # return candidates
 
 def chose_speaker(candidates):
-#this looks at the final scores of candidates and pick the highest
+    #this looks at the final scores of candidates and pick the highest
+    #called by: find_speaker
     max_votes=0
     max_index=0
     for i in range (len(candidates)):
@@ -93,21 +125,23 @@ def chose_speaker(candidates):
     #for testing, it will return the max index given that the candidates index will evenly match up with the first frame index. 
     return max_index
 
-
-def find_speaker(video_path):
+#this is the rework version
+#def find_speakear(video_path):
+    
+def find_speaker_single(video_path):
     #this version is to be integrated with the rest of the script
     print("starting")
     print(video_path)
     cap = cv.VideoCapture(video_path)
     img_path = video_path+".image.jpg"
 
-
+    '''
     ret, frame = cap.read()
 
     cv.imwrite(img_path, frame)
     #cv.imshow('input frame', frame)
     faces = extract(img_path)
-
+    '''
 
 
     ret, frame = cap.read()
@@ -118,9 +152,115 @@ def find_speaker(video_path):
     cap.release()
     os.remove(img_path)
 
+    
+    if len(candidates)==1:
+        return candidates[0][0]
+    else:
+        return "no"
+
+    #the below segment is commented out to only keep 1 speaker
+
+def find_speaker_test(video_path):
+    print("starting")
+    print(video_path)
+    cap = cv.VideoCapture(video_path)
+    img_path = video_path+".image.jpg"
+
+    ret, frame = cap.read()
+    cv.imwrite(img_path, frame)
+    candidates = extract_test(img_path)
+    
+    
+    os.remove(img_path)
+
+    if len(candidates)>1:
+        print("frame 0, multiple person")
+        status = "multiple"
+    elif len(candidates)==0:
+        print("frame 0, no speaker")
+        status = "no"
+    elif candidates[0][2]<200 or candidates[0][3]<200:
+        print("frame 0, too small")
+        status = "small"
+    else:
+        status = "pass"
+        candidate = candidates[0]
+
+    
+    frame_num = cap.get(cv.CAP_PROP_FRAME_COUNT)
+    
+    
+    output_path = video_path + ".temp.mp4"
+
+    frame_width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+    fourcc = cv.VideoWriter_fourcc(*'mp4v')
+    out = cv.VideoWriter(output_path, fourcc, 25, (frame_width, frame_height))
+    print("frame num: " + str(int(frame_num)))
+    time1 = time.perf_counter()
+    for i in range(1, int(frame_num)):
+        ret, frame = cap.read()
+        if not ret:
+            break
+        img_path = video_path+".temp.jpg"
+        if(not status=="pass"):
+            out.write(frame)
+            continue
+        
+
+        cv.imwrite(img_path, frame)
+        faces=extract_test(img_path) 
+        if len(faces)>1:
+            status = "multiple"
+            out.write(frame)
+            print("frame "+str(i)+" multiple faces")
+        elif len(faces)==0 or not compare_face(faces[0][0], candidate[0]):
+            status = "left"
+            out.write(frame)
+            print("frame "+str(i)+" speaker left")
+        elif faces[0][2]<200 or faces[0][3]<200:
+            status = "small"
+            out.write(frame)
+            print("frame "+str(i)+" face too small")
+        else:
+            location = faces[0][1]
+            cv.rectangle(frame, (location['x'], location['y']), (location['x']+location['w'], location['y']+location['h']), (0, 0, 255), 2)
+            out.write(frame)
+        os.remove(img_path)
+       
+        if((i+1)%25==0 or (i+1)==frame_num):
+            time2=time.perf_counter()
+            print("processed frames: "+str(i+1))
+            print("time: "+str(time2-time1))
+
+    os.remove(video_path)
+    os.rename(output_path, video_path+"."+status+".mp4")
+    cap.release()
+    out.release()
+
+
+os.chdir("/Users/clarencewu/Downloads")
+folder = "avspeech_part_c1"
+
+directory = os.getcwd()
+path = os.path.join(directory, folder)
+print(f"Current directory: {directory}")
+print(f"Folder path: {path}")
+video_files = get_video_files(path)
+print(len(video_files))
+for i in range(len(video_files)):
+    print("running video "+str(i))
+    video=video_files[i]
+    find_speaker_test(video)
+    print("")
+    
+        
+    '''
     cap = cv.VideoCapture(video_path)
     print("filtering candidates")
     for i in range(10):
+        if len(candidates) ==1:
+            break
         ret, frame = cap.read()
         if not ret:
             break
@@ -140,6 +280,8 @@ def find_speaker(video_path):
 
     #identify which face belongs to the speaker. the largest face is assumed to be the speaker's
     #the above idea is scratched
+    '''
+
     '''
     max_size=0
     max_index=-1
